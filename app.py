30c847180643ebe01281c7b949c01b0fe8c72174
mohith <mohithgundemeda@gmail.com>
2023-10-02 10:33:53 +0530

    Remove requirement.txt

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask,request,app,jsonify,url_for,render_template
+import pickle
+import numpy
+import pandas
+app=Flask(__name__)
+cl_model=pickle.load(open("telecom_model.pkl", 'rb'))
+scaler=pickle.load(open('scaling.pkl','rb'))
+@app.route('/')
+def home():
+    return render_template('home.html')
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(numpy.array(list(data.values())).reshape(1,-1))
+    new_data=scaler.transform(numpy.array(list(data.values())).reshape(1,-1))
+    output=cl_model.predict(new_data)
+    return jsonify(output[0])
+if __name__=="__main__":
+    app.run(debug=True)
