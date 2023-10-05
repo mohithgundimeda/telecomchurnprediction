75b50566c066c9b7e4a1dd10b5fa207da4630dc1
mohith <mohithgundemeda@gmail.com>
2023-10-05 09:05:36 +0530

    the web app is ready

--- app.py
+++ app.py
@@ -16,5 +16,13 @@ def predict_api():
     new_data=scaler.transform(numpy.array(list(data.values())).reshape(1,-1))
     output=cl_model.predict(new_data)
     return jsonify(output[0])
+@app.route('/predict',methods=['POST'])
+def predict():
+    data_list= request.form.values()
+    data=list(data_list)
+    new_data=scaler.transform(numpy.array(data).reshape(1,-1))
+    print(new_data)
+    output=cl_model.predict(new_data)[0]
+    return render_template("home.html",predict_text="the predicted coustomer status is {}".format(output))
 if __name__=="__main__":
     app.run(debug=True)
